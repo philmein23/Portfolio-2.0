3fbffb8384e67431871ce7d7e7d1af6f580afa77
Phil Nguyen <philnguyen@Phils-MBP.home>
2016-02-01 20:41:24 -0800

    added styling, markup and js

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+var projects = [
+    {
+        title: 'Portfolio',
+        submittedOn: '2016-2-1',
+        projectDetails: '<p>Web Developer Portfolio showcasing all projects using a variety of languages utilizng HTML, CSS, and Javascript</p>'
+
+    }
+
+
+
+
+]
