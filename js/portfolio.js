783e86aa3548e84015b12200c905ba93837e78bc
Phil Nguyen <philnguyen@Phils-MBP.home>
2016-02-01 22:02:57 -0800

    added styling module, javascript behavior to append template

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,46 @@
-var projects = [
-    {
-        title: 'Portfolio',
-        submittedOn: '2016-2-1',
-        projectDetails: '<p>Web Developer Portfolio showcasing all projects using a variety of languages utilizng HTML, CSS, and Javascript</p>'
+var projects = [];
 
-    }
+function Project (data) {
+
+    this.title = data.title;
+    this.submittedOn = data.submittedOn;
+    this.projectURL = data.projectURL;
+    this.projectDetails = data.projectDetails;
+
+}
+
+Project.prototype.toHtml = function() {
+
+    var $newProject = $('article.template').clone();
 
 
+    $newProject.find("header h2").text(this.title);
+    $newProject.find("time").attr('datatime', this.submittedOn);
+    $newProject.attr('href', this.projectURL);
+    $newProject.find(".project-description").html(this.projectDetails);
+
+    $newProject.find('time').html('about' + parseInt((new Date() - new Date(this.submittedOn))/60/60/24/1000) + ' days ago');
 
 
-]
+    $newProject.removeClass('template');
+
+   return $newProject;
+
+}
+
+rawData.sort(function(a,b) {
+
+    return (new Date(b.submittedOn)) - (new Date(a.submittedOn));
+
+})
+
+rawData.forEach(function(element) {
+    projects.push(new Project(element));
+
+});
+
+projects.forEach(function(a) {
+
+    $('#projects').append(a.toHtml());
+
+})
---
+++ js/projects.js
@@ -0,0 +1,13 @@
+var rawData = [
+    {
+        title: 'Portfolio',
+        submittedOn: '2016-2-1',
+        projectURL: 'https://github.com/philmein23/PhillipPortfolio2',
+        projectDetails: '<p>Web Developer Portfolio showcasing all projects using a variety of languages utilizng HTML, CSS, and Javascript</p>'
+
+    }
+
+
+
+
+]
