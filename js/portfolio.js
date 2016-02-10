c8b2a094964c16f9b01fb7c7580497fbdbc4989f
Phil Nguyen <philnguyen@Phils-MBP.home>
2016-02-09 23:46:19 -0800

    added css styling, new img, functional programming

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,76 +1,82 @@
-function Project(data) {
+(function (module) {
 
-    this.title = data.title;
-    this.language = data.language;
-    this.category = data.category;
-    this.submittedOn = data.submittedOn;
-    this.projectURL = data.projectURL;
-    this.projectDetails = data.projectDetails;
+    function Project(data) {
 
-}
+        this.title = data.title;
+        this.language = data.language;
+        this.category = data.category;
+        this.submittedOn = data.submittedOn;
+        this.projectURL = data.projectURL;
+        this.projectDetails = data.projectDetails;
 
-Project.all = [];
+    }
 
-Project.prototype.toHtml = function () {
+    Project.all = [];
 
-
-    var template = Handlebars.compile($('#project-template').text())
-
-    this.daysAgo = parseInt((new Date() - new Date(this.submittedOn)) / 60 / 60 / 24 / 1000);
-    this.submitDate = this.submittedOn ? 'submitted ' + this.daysAgo + ' days ago' : '(draft)';
-
-    return template(this);
-
-};
+    Project.prototype.toHtml = function () {
 
 
-Project.loadAll = function (rawData) {
-    rawData.sort(function (a, b) {
+        var template = Handlebars.compile($('#project-template').text())
 
-        return (new Date(b.submittedOn)) - (new Date(a.submittedOn));
+        this.daysAgo = parseInt((new Date() - new Date(this.submittedOn)) / 60 / 60 / 24 / 1000);
+        this.submitDate = this.submittedOn ? 'submitted ' + this.daysAgo + ' days ago' : '(draft)';
 
-    })
+        return template(this);
 
-    rawData.forEach(function (data) {
-        Project.all.push(new Project(data));
-
-    });
+    };
 
 
-};
+//The parameter is an array of objects so why would the constructor function use an array of objects as an argument to instantiate a new object?
+    Project.loadAll = function (rawData) {
+        rawData.sort(function (a, b) {
 
-Project.fetchAll = function () {
-    if (localStorage.rawData) {
+            return (new Date(b.submittedOn)) - (new Date(a.submittedOn));
 
-        $.ajax({
-            type: 'HEAD',
-            url: 'data/project.json',
-            success: function (data, message, xhr) {
-                console.log(xhr);
-
-                var eTag = xhr.getResponseHeader('eTag');
-                if (!localStorage.eTag || eTag !== localStorage.eTag) {
-                    localStorage.eTag = eTag;
-                } else {
-                    Project.loadAll(JSON.parse(localStorage.rawData));
-                }
-            }
         })
-        Project.loadAll(JSON.parse(localStorage.rawData));
-        projectView.initiateIndexPage();
 
+        Project.all = rawData.map(function (ele) {
+            return new Project(ele);
+        })
 
-    } else {
-        $.getJSON('data/project.json', function(rawData) {
+    };
+
+    Project.getAll = function () {
+        $.getJSON('data/project.json', function (rawData) {
             localStorage.rawData = JSON.stringify(rawData);
             Project.loadAll(JSON.parse(localStorage.rawData));
             projectView.initiateIndexPage();
         })
 
+    }
+
+    Project.fetchAll = function () {
+        if (localStorage.rawData) {
+
+            $.ajax({
+                type: 'HEAD',
+                url: 'data/project.json',
+                success: function (data, message, xhr) {
+                    console.log(xhr);
+
+                    var eTag = xhr.getResponseHeader('eTag');
+                    if (!localStorage.eTag || eTag !== localStorage.eTag) {
+                        localStorage.eTag = eTag;
+                        Project.getAll();
+                    } else {
+                        Project.loadAll(JSON.parse(localStorage.rawData));
+                        projectView.initiateIndexPage();
+                    }
+                }
+            })
+
+        } else {
+            Project.getAll();
+        }
+
 
     };
 
-
-}
+    module.Project = Project;
+})(window);
 
 
