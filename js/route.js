ff126d7f76bf1481d1eaac69f090427987929e45
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-03-29 00:12:29 -0700

    removed unused scripts

--- js/route.js
+++ js/route.js
@@ -1,6 +1,5 @@
 //Configure routes for each controller
 page('/', projectController.loadAll, projectController.index);
 page('/about', aboutController.index);
-page('/current', currentProjectController.index);
 
 page(); //Activates page.js
